bc00a35450f7da7fdbd93588fb683abee46c54f8
josspheenboles <eng.jospheenboles@gmail.com>
2025-02-26 20:12:35 +0200

    Day1

---
+++ demoday1.py
@@ -0,0 +1,122 @@
+#array collection of values same data type
+import  array
+arr=array()
+# x,y=10,20
+# print(x,y)
+# x,y=y,x
+# print(x,y)
+# num=10
+# print('aya','ali',num,sep='&')
+# aya&ali&10
+#unpacking concept
+# name,age='sara',23
+# name='ali'
+# print(name)
+# print(name+' '+str(age))
+# print(name,age,78,sep='&',end='===>')
+# print('2',22)
+# print('jjj')
+
+#collection of chat
+# s='Hello {n},you in track {tr}, {n}'
+# print(s.format(tr='iot',n='sara'))
+# print(s.format('aya','python'))
+# print(s.format('ali','iot'))
+# print(s.format('php','sara'))
+# # s='aya ali'
+# print(s)
+# # s[[start=0]:[end=collection lenght]:[step=1]]
+# print(s[1:5])
+# print(s[::-1])
+# print(s[0],s[-7])
+# name='AYA'#input('enter your full name')
+# print(name.swapcase())
+# print(name.capitalize())
+# print(name.lower())
+# print(name.upper())
+# print(name.endswith('A'))
+#
+# print(name)
+# print(name.replace('ay','@',3))
+# print(name.split('y'))
+# print(name)
+# age=input('enter your age')
+# if age.isdigit() and age.isspace():
+#     age=int(age)
+# else:
+#     print('invalid age must be number')
+# c1=3+2j
+# c2=5+3j
+# print(c1/c2)
+# print(c1,type(c1))
+# #and return last truely or first falsy
+# #or return first truely or last falsy
+# val1 and val2 and val3---->val3
+# val1 or val2 or val3--->val3
+# print(2 and 5)#
+# # #join condition
+# # print(1==1 or 3!=3)
+# # print(not True)
+# # # print(5=='5') #== compare value  & data type
+# # # print(True==1)
+# # # print(False==0)
+# # # # print(16//5)
+# # # # print(2**4)
+# # # # # name=inti trainee {age}')
+# # # # # #multi line str==>''' ''',""" """
+# # # # # 'aya'
+# # # # # """
+# # # # # bio='''name:aya
+# # # # # id=10'''
+# # # # # bio='name:aya \nid:11'
+# # # # # print(bio)
+# # # # # """
+# # # # # # #single line of str---> '',""
+# # # # # # fname='aya'
+# # # # # # fullname=f"{fname} ali"#"{fname} ali"
+# # # # # # print(fullname)
+# # # # # # #code block1
+# # # # # # number=1 #palaplpal
+# # # # # # #code block1
+# # # # # # number2=2
+# # # # # # if number==number2:
+# # # # # #     #code block2
+# # # # # #     print('plpl')
+# # # # # #     print('plplpl1')
+# # # # # # print('plplpl')
+# # # # # #
+# # # # # # #block level1
+# # # # # # Num=1
+# # # # # # num=2
+# # # # # # if True:
+# # # # # #     #block level 2
+# # # # # #     print('1')
+# # # # # # print('2')
+# # # # # # print(num,Num)
+# # # # # # # first_n3ame=input('enter first name')
+# # # # # # # #python is loosly typed pg
+# # # # # # # #python dynmicly typed
+# # # # # x=1#auto deteect x is int
+# # # # # print(type(x))
+# # # # # x='one'#object of str
+# # # # # print(type(x))
+# # # # # x=1.1
+# # # # # print(type(x))
+# # # # # x=True
+# # # # # print(type(x))
+# # # # # x=[]
+# # # # # print(type(x))
+# # # # # # x=()
+# # # # # # print(type(x))
+# # # # # # x={}
+# # # # # # print(type(x))
+# # # # # #
+# # # # # # # print('hi python tracl')
+# # # # # # #variable--->data type
+# # # # # # #primitive
+# # # # # # # int,float,str,bool
+# # # # # # #non prim
+# # # # # # # array ,list,tuple,set,dict
+# # # # # # #calcauate --->max size
+# # # # # # # import sys
+# # # # # # # print(sys.maxsize)
