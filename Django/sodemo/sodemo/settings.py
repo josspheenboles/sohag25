3d9b72d31b273c5375e252ddfaad803d40e84335
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-08 10:15:25 +0200

    add apps to sttings professional

--- Django/sodemo/sodemo/settings.py
+++ Django/sodemo/sodemo/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'track',
-    'trainee',
-    'myuser'
-
+    #prof
+    'track.apps.TrackConfig',#'track',#not prof
+    'trainee.apps.TraineeConfig',#'trainee',
+    'myuser.apps.MyuserConfig'
 ]
 
 MIDDLEWARE = [
