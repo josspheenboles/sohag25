a73d09fe58fcc3df3ee0a3f7c732c093abe1d9d2
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-08 11:05:12 +0200

    routes pass int

--- Django/sodemo/track/views.py
+++ Django/sodemo/track/views.py
@@ -6,5 +6,5 @@ def Listview(req):
     return HttpResponse('<h1>Tracks</h1>')
 def Addview(req):
     return HttpResponse('<h1>add Track</h1>')
-def Updateview(req):
+def Updateview(req,id):
     return HttpResponse('<h1>Updateview Track</h1>')
