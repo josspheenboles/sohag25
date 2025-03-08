41a2f8077f153ec7bf488ff3fc3e887b90b21160
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-08 11:09:01 +0200

    routes pass str

--- Django/sodemo/sodemo/urls.py
+++ Django/sodemo/sodemo/urls.py
@@ -28,5 +28,8 @@ urlpatterns = [
     path('Track/',Listview,name='tracks'),
     path('Track/Add',Addview,name='tracknew'),
     path('Track/Update/<int:id>',Updateview,name='trackupdate'),
+    path('Track/Delete/<int:id>',Deleteview,name='trackdelete'),
+    path('Track/<str:name>',findview,name='trackfind'),
+
 
 ]
--- Django/sodemo/track/views.py
+++ Django/sodemo/track/views.py
@@ -8,3 +8,7 @@ def Addview(req):
     return HttpResponse('<h1>add Track</h1>')
 def Updateview(req,id):
     return HttpResponse(f'<h1>Updateview Track {id}</h1>')
+def Deleteview(req,id):
+    return HttpResponse(f'<h1>Deleteview Track {id}</h1>')
+def findview(req,name):
+    return HttpResponse(f'<h1>findview Track {name}</h1>')
