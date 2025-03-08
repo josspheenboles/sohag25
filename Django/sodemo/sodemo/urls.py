748348a4acbd338f7be5334bf2d15fb5a54ca75e
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-08 11:13:49 +0200

    prtial urls

--- Django/sodemo/sodemo/urls.py
+++ Django/sodemo/sodemo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from myuser.views import *
 from track.views import *
 urlpatterns = [
@@ -25,5 +25,5 @@ urlpatterns = [
     path('Logout/',logoutview,name='Logout'),
     path('Reg/',Regview,name='Reg'),
     #routes track app
-
+    path('Track/',include('track.urls')),
 ]
