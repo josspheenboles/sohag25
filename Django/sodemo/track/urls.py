6753a65a88fc06bb98aec142efc509564b7e6146
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-08 11:29:01 +0200

    tempates

--- Django/sodemo/track/urls.py
+++ Django/sodemo/track/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
-urlpatterns=[path('Track/',Listview,name='tracks'),
+urlpatterns=[
+    path('',Listview,name='tracks'),
     path('Add',Addview,name='tracknew'),
     path('Update/<int:id>',Updateview,name='trackupdate'),
     path('Delete/<int:id>',Deleteview,name='trackdelete'),
--- Django/sodemo/track/views.py
+++ Django/sodemo/track/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def Listview(req):
-    return HttpResponse('<h1>Tracks</h1>')
+    # return HttpResponse('<h1>Tracks</h1>')
+    return render(req,'track/list.html')
 def Addview(req):
-    return HttpResponse('<h1>add Track</h1>')
+    # return HttpResponse('<h1>add Track</h1>')
+    return render(req,'track/new.html')
 def Updateview(req,id):
     return HttpResponse(f'<h1>Updateview Track {id}</h1>')
 def Deleteview(req,id):
