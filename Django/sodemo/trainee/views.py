fe1c39a846c8b08443db5648923ee637727c2670
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-10 11:01:48 +0200

    trainee urls

--- Django/sodemo/trainee/views.py
+++ Django/sodemo/trainee/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def getalltrainees(req):
     return render(req,'trainee/list.html')
+def addtrainees(req):
+    return render(req,'trainee/add.html')
