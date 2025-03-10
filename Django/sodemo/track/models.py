ebdef3973517594781cc9545dd7980d9ce19e393
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-10 10:05:41 +0200

    db migrate

--- Django/sodemo/track/models.py
+++ Django/sodemo/track/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Track(models.Model):
     #prop
     #id int autoincrement pk
-    id=models.AutoField(primary_key=True,db_column='trackid')
+    id=models.AutoField(primary_key=True)
     #name varchar 50
     name=models.CharField(max_length=50,null=False,unique=True)
