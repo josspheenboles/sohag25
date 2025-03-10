d2204ab8f182161568390f8232a895148ab3a361
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-10 11:40:00 +0200

    add trainee

--- Django/sodemo/trainee/views.py
+++ Django/sodemo/trainee/views.py
@@ -5,13 +5,17 @@ def getalltrainees(req):
     return render(req,'trainee/list.html')
 def addtrainees(req):
     if(req.method=='POST'):
-        #get info
-        trname=req.POST['trname']
-        tremail=req.POST['tremail']
-        trimg=req.FILES['trimg']
-        #save db
-        obj=Trainee(name=trname,email=tremail,image=trimg)
-        obj.save()
+        # #get info
+        # trname=req.POST['trname']
+        # tremail=req.POST['tremail']
+        # trimg=req.FILES['trimg']
+        # #save db
+        # obj=Trainee(name=trname,email=tremail,image=trimg)
+        # obj.save()
+        Trainee.objects.create(name=req.POST['trname']
+                               ,email=req.POST['tremail']
+                               ,image=req.FILES['trimg']
+                               )
     return render(req,'trainee/add.html')
 def updatetrainees(req,id):
     return render(req, 'trainee/update.html')
