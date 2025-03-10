b042b5065a40a5751dff7d2f19679f188b2f129d
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-10 11:09:32 +0200

    trainee model

--- Django/sodemo/trainee/models.py
+++ Django/sodemo/trainee/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Trainee(models.Model):
+    #id auto,name,image,creatdate,email
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    #image path upload image to it &
+    # image=models.ImageField()
+    createdate=models.DateTimeField(auto_now_add=True)
+    updateddate=models.DateTimeField(auto_now=True)
--- Django/sodemo/trainee/urls.py
+++ Django/sodemo/trainee/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 urlpatterns=[
     path('',getalltrainees,name='trall'),
-    path('/Add',addtrainees,name='tradd'),
-    path('/Update/<int : id>',updatetrainees,name='trupdate'),
-    path('/Delete/<int : id>',deletetrainees,name='trdelete'),
+    path('Add',addtrainees,name='tradd'),
+    path('Update/<int:id>',updatetrainees,name='trupdate'),
+    path('Delete/<int:id>',deletetrainees,name='trdelete'),
 
 ]
