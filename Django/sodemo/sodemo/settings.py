82e576644022127945b5a8d85da25f965b9902dd
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-10 09:57:58 +0200

    db confg & track model

--- Django/sodemo/sodemo/settings.py
+++ Django/sodemo/sodemo/settings.py
@@ -81,8 +81,15 @@ WSGI_APPLICATION = 'sodemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #enginge postgres
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demosoh',
+        'USER':'postgres',#production noooooooooooo
+        'PASSWORD':'123',
+        'HOST':'localhost',#best parc.
+        'PORT':5432,
+
+
     }
 }
 
--- Django/sodemo/track/models.py
+++ Django/sodemo/track/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+#model Track
+class Track(models.Model):
+    #prop
+    #id int autoincrement pk
+    id=models.AutoField(primary_key=True,db_column='trackid')
+    #name varchar 50
+    name=models.CharField(max_length=50,null=False,unique=True)
