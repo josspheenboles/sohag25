8a7ad1235106ac8fcbc5bf9357a658ed5225038f
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-10 11:37:23 +0200

    image upload

--- Django/sodemo/trainee/views.py
+++ Django/sodemo/trainee/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-
+from .models import Trainee
 # Create your views here.
 def getalltrainees(req):
     return render(req,'trainee/list.html')
 def addtrainees(req):
+    if(req.method=='POST'):
+        #get info
+        trname=req.POST['trname']
+        tremail=req.POST['tremail']
+        trimg=req.FILES['trimg']
+        #save db
+        obj=Trainee(name=trname,email=tremail,image=trimg)
+        obj.save()
     return render(req,'trainee/add.html')
 def updatetrainees(req,id):
     return render(req, 'trainee/update.html')
